584a48d4483e4f69dae17e5e66a5a78dd9ee0561
ngoctrangnguyen1701 <89184893+ngoctrangnguyen1701@users.noreply.github.com>
2021-10-05 15:18:07 +0700

    commit

--- src/Pages/Page404/Page404.js
+++ src/Pages/Page404/Page404.js
@@ -10,7 +10,7 @@ function Page404(props) {
                 <img src="images/404/404.png" className="img-responsive" alt="" />
                 <h1><b>OPPS!</b> We Couldn’t Find this Page</h1>
                 <p>Uh... So it looks like you brock something. The page you are looking for has up and Vanished.</p>
-                <h2><Link href="/">Bring me back Home</Link></h2>
+                <h2><Link to="/">Bring me back Home</Link></h2>
             </div>
         </div>
     );
